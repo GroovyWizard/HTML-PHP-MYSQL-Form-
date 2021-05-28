ddd6650f0cfe144dd1ad64ac63dc421a343360c7
Nekrobutcher <tardyfox@gmail.com>
2021-05-28 20:18:17 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+	<body> 
+	<link rel="stylesheet" href="style.css">
+	<div class="container">
+		<div class="left">
+			<div class="header">
+				<h2 class="animation a1">Bem vindo de volta</h2>
+				<h4 class="animation a2">Qual seu email e Nome de Usuário?</h4>
+			</div>
+			<form action="welcome_get.php" method="get"  class="form">
+				<input type="text" name="email" class="form-field animation a3" placeholder="Email"> <br>
+				<input type="text" name="name" class="form-field animation a4" placeholder="Usuario"> <br>
+				<p class="animation a5">
+					<a href="#" class="forgotusr">Esqueceu o seu nome de usuário?</a></p>
+				<input type="submit" class="animation a6">
+				</form>
+			</div>
+		</div>
+		<div class="right"></div>
+	
+	
+	
+	
+	</body>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+</html>
+
+
+
+
+<?php
+
+?>
