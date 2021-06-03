bb4adef9359af9c26fa478acc99998a73da04b22
Nekrobutcher <tardyfox@gmail.com>
2021-06-03 14:32:35 -0300

    23293902302

---
+++ site.php
@@ -0,0 +1,14 @@
+ <html>
+	<body>
+		<link rel="stylesheet" href="style.css">
+		
+		
+		
+		<form action="validate.php" method="get">
+			Name: <input type="text" name="name"><br>
+			E-mail: <input type="text" name="email"><br>
+			<input type="submit">
+		</form>
+
+	</body>
+</html> 
